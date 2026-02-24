b1651893d4678104e0244d520fc5881141be3215
tomic851 <tomicmejor1@gmail.com>
2026-02-24 10:55:23 +0300

    commit 2

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import pymysql
 import pymysql.cursors
 import os # Its going to allow python code to communicate with the operating system  
 # create flask app
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 #configure upload folder
 app.config['UPLOAD_FOLDER'] = 'static/images'
 
@@ -99,8 +101,73 @@ def get_product_details():
 
     product_details = cursor.fetchall()
     return jsonify(product_details)
+#  M-pesa payment
+import requests
+import datetime
+import base64
+from requests.auth import HTTPBasicAuth
 
-    
+@app.route('/api/mpesa_payment', methods=['POST'])
+def mpesa_payment():
+    if request.method == 'POST':
+        # Extract POST Values sent
+        amount = request.form['amount']
+        phone = request.form['phone']
+
+        # Provide consumer_key and consumer_secret provided by safaricom
+        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+        consumer_secret = "amFbAoUByPV2rM5A"
+
+        # Authenticate Yourself using above credentials to Safaricom Services, and Bearer Token this is used by safaricom for security identification purposes - Your are given Access
+        api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"  # AUTH URL
+        # Provide your consumer_key and consumer_secret 
+        response = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+        # Get response as Dictionary
+        data = response.json()
+        # Retrieve the Provide Token
+        # Token allows you to proceed with the transaction
+        access_token = "Bearer" + ' ' + data['access_token']
+
+        #  GETTING THE PASSWORD
+        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')  # Current Time
+        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'  # Passkey(Safaricom Provided)
+        business_short_code = "174379"  # Test Paybile (Safaricom Provided)
+        # Combine above 3 Strings to get data variable
+        data = business_short_code + passkey + timestamp
+        # Encode to Base64
+        encoded = base64.b64encode(data.encode())
+        password = encoded.decode()
+
+        # BODY OR PAYLOAD
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password":password,
+            "Timestamp": timestamp,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": "1",  # use 1 when testing
+            "PartyA": phone,  # change to your number
+            "PartyB": "174379",
+            "PhoneNumber": phone,
+            "CallBackURL": "https://coding.co.ke/api/confirm.php",
+            "AccountReference": "SokoGarden Online",
+            "TransactionDesc": "Payments for Products"
+        }
+
+        # POPULAING THE HTTP HEADER, PROVIDE THE TOKEN ISSUED EARLIER
+        headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+
+        # Specify STK Push  Trigger URL
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"  
+        # Create a POST Request to above url, providing headers, payload 
+        # Below triggers an STK Push to the phone number indicated in the payload and the amount.
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text) # 
+        # Give a Response
+        return jsonify({"message": "An MPESA Prompt has been sent to Your Phone, Please Check & Complete Payment"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
